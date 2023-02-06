e8e4391635a77c21636ac237260ffc3e3530d058
Andrei <andrei_ionut@rocketmail.com>
2023-02-06 21:05:13 +0200

    first git

--- src/main/java/org/example/Intro.java
+++ src/main/java/org/example/Intro.java
@@ -1,9 +1,10 @@
 package org.example;
 
-//import java.util.Scanner;
+import java.util.Scanner;
+
 public class Intro {
     public static void main(String[] args) {
-       /*
+
         System.out.println("Hello");
 
         Scanner input = new Scanner(System.in);
@@ -11,7 +12,7 @@ public class Intro {
 
         String name = input.nextLine();
         System.out.println("Welcome to the Java ccourse," + name);
-        */
+
         System.out.println("text with a quote \"quote\"");
         System.out.println("The project is in C:\\Users");
         System.out.println("Text with blank space:  and other    text after");
